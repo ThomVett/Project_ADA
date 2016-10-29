8b0bcfc3bf97a4a8b6629cddfbf27dc64039fb3f
adderbyte <adderbyte@users.noreply.github.com>
2016-10-29 06:53:45 +0200

    Add files via upload

---
+++ plots.py
@@ -0,0 +1,20 @@
+import matplotlib.plot as plt
+import seaborn as sns
+from bokeh.plotting import figure, output_file, show
+import pandas as pd
+
+
+
+def plotname():
+    
+    '''
+    Place comments here.
+    This  be for imortant plots.
+    
+    '''
+    
+    raise NotImplementedError
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+
+def functions():
+    
+    '''
+    Templates for the project. This docstring enable 'help(utils.functions)' to return the comments 
+    placed in this docstring. 
+    
+    Placed Comments about Functions here. Specify in put and output.
+    
+    Input:
+    
+    
+    Output:
+   
+    '''
+    
+    raise NotImplementedError
